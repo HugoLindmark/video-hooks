99bc2c27d92d2761e306cad4688ad6ba096a051a
Hugo Lindmark <hugo.lindmark@gmail.com>
2021-01-11 09:27:32 +0100

    testing stuff

--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -4,4 +4,5 @@ SHA := git rev-parse --short HEAD
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker tag docker-test htotheo/docker-test:$(SHA)
 docker tag docker-test htotheo/docker-test:latest
-docker push htotheo/docker-test
+docker push htotheo/docker-test:latest
+docker push htotheo/docker-test:$(SHA)
